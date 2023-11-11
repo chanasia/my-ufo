b8b8d45c1e8d3484c110725ffc8971e7544ebfec
chanasia <asiavip2546@gmail.com>
2023-11-11 14:05:41 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import { NextPage } from "next"
+import { unstable_noStore as noStore } from 'next/cache'
 
-type Props = {}
+type Props = {
+}
 
-const Home:NextPage = ({}: Props) => {
+export default function Home({}: Props) {
+  noStore()
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="flex flex-col text-lg text-center gap-4 select-none">
@@ -10,12 +13,9 @@ const Home:NextPage = ({}: Props) => {
         <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/memo">Memo</a>
         <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href={
           process.env.NODE_ENV === "production" ?
-          `http://resume.${process.env.HOST}` : "#"
+          `http://resume.${process.env.HOSTNAME}` : "#"
         }>Resume</a>
       </div>
     </div>
   )
 }
-
-
-export default Home
