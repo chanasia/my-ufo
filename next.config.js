71389fca1598d04eb5c73940f01a998a4876cdac
chanasia <asiavip2546@gmail.com>
2023-11-11 14:05:56 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    HOST: process.env.HOST,
-    PORT: process.env.PORT,
+    HOSTNAME: process.env.HOSTNAME,
   },
 }
 
