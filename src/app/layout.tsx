07cca04faaccbd48a668861b3ee65a127db97886
chanasia <asiavip2546@gmail.com>
2023-11-11 21:12:10 +0700

    ✨ feat: Add both icon (jsonicon, darkicon)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <div className="absolute top-0 left-0 w-full flex justify-center items-center text-center select-none">
-          <div className="py-4 text-xl w-5/6 mx-auto border-b border-gray-500">
-            CHANASIA
-          </div>
-        </div> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { unstable_noStore as noStore } from 'next/cache'
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import DataObjectIcon from '@mui/icons-material/DataObject';
 
 type Props = {
 }
@@ -7,14 +9,23 @@ export default function Home({}: Props) {
   noStore()
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="flex flex-col text-lg text-center gap-4 select-none">
-        <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/projects">Projects</a>
-        <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/memo">Memo</a>
-        <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href={
-          process.env.NODE_ENV === "production" ?
-          `http://resume.${process.env.HOSTNAME}` : "#"
-        }>Resume</a>
+    <div className='relative'>
+      <div className="absolute top-0 left-0 w-full flex justify-center items-center select-none bg-black">
+          <div className="py-8  w-3/5 mx-auto border-b border-gray-400 flex justify-around">
+            <DataObjectIcon className='text-3xl' />
+            <h1 className='text-xl w-[150px] text-center'>CHANASIA</h1>
+            <button><DarkModeIcon className='text-3xl' /></button>
+          </div>
+        </div>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="flex flex-col text-lg text-center gap-4 select-none">
+          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/projects">Projects</a>
+          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/memo">Memo</a>
+          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href={
+            process.env.NODE_ENV === "production" ?
+            `http://resume.${process.env.HOSTNAME}` : "#"
+          }>Resume</a>
+        </div>
       </div>
     </div>
   )
