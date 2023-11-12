97a8d516eebc43ab2a44c181cdcca7e455bfff81
chanasia <asiavip2546@gmail.com>
2023-11-12 10:18:18 +0700

    ✨ feat:add transition between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,48 @@
+"use client"
 import { unstable_noStore as noStore } from 'next/cache'
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import DataObjectIcon from '@mui/icons-material/DataObject';
+import { motion } from 'framer'
+import Link from 'next/link';
 
 type Props = {
 }
 
-export default function Home({}: Props) {
+export default function Home({ }: Props) {
   noStore()
 
   return (
-    <div className='relative'>
+    <div className='relative overflow-hidden h-full'>
       <div className="absolute top-0 left-0 w-full flex justify-center items-center select-none bg-black">
-          <div className="py-8  w-3/5 mx-auto border-b border-gray-400 flex justify-around">
-            <DataObjectIcon className='text-3xl' />
-            <h1 className='text-xl w-[150px] text-center'>CHANASIA</h1>
-            <button><DarkModeIcon className='text-3xl' /></button>
-          </div>
-        </div>
-      <div className="w-full h-screen flex justify-center items-center">
-        <div className="flex flex-col text-lg text-center gap-4 select-none">
-          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/projects">Projects</a>
-          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/memo">Memo</a>
-          <a className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href={
-            process.env.NODE_ENV === "production" ?
-            `http://resume.${process.env.HOSTNAME}` : "#"
-          }>Resume</a>
+        <div className="py-8 text-xl w-3/5 mx-auto border-b border-gray-400 flex justify-around text-center">
+          <Link href="#" className='cursor-default'><DataObjectIcon className='text-3xl w-[24px] h-[24px]' /></Link>
+          <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{ duration: 0.3 }}  
+          >
+          <h1 className='text-xl w-[150px]'>CHANASIA</h1>
+          </motion.div>
+          <button><DarkModeIcon className='text-3xl w-[24px] h-[24px]' /></button>
         </div>
       </div>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -20, opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="w-full h-screen flex justify-center items-center">
+          <div className="flex flex-col text-lg text-center gap-4 select-none">
+            <Link className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/projects">Projects</Link>
+            <Link className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href="/memo">Memo</Link>
+            <Link className="border rounded-lg py-4 px-20 hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200" href={
+              process.env.NODE_ENV === "production" ?
+                `http://resume.${process.env.HOSTNAME}` : "#"
+            }>Resume</Link>
+          </div>
+        </div>
+      </motion.div>
     </div>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,48 +1,65 @@
+"use client"
 import LaunchIcon from '@mui/icons-material/Launch';
 import DataObjectIcon from '@mui/icons-material/DataObject';
 import './style.scss'
 import UndoIcon from '@mui/icons-material/Undo';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { motion } from 'framer'
+import Link from 'next/link'
 
 type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div>
+    <div className='relative overflow-hidden h-full'>
       <div className="absolute top-0 left-0 w-full flex justify-center items-center select-none bg-black">
-        <div className="py-8 text-xl w-3/5 mx-auto border-b border-gray-400 flex justify-around">
-          <a href="/"><UndoIcon className='text-3xl' /></a>
-          <h1 className='text-xl w-[150px] text-center'>My Projects</h1>
-          <button><DarkModeIcon className='text-3xl' /></button>
+        <div className="py-8 text-xl w-3/5 mx-auto border-b border-gray-400 flex justify-around text-center">
+          <Link href="/"><UndoIcon className='text-3xl w-[24px] h-[24px]' /></Link>
+          <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{ duration: 0.3 }}   
+          >
+            <h1 className='text-xl w-[150px]'>My Projects</h1>
+          </motion.div>
+          <button><DarkModeIcon className='text-3xl w-[24px] h-[24px]' /></button>
         </div>
       </div>
-
-      <div className="mt-[150px] w-3/5 mx-auto grid grid-cols-3 gap-4">
-        <div className='border border-dashed rounded-lg p-4 h-[109px]'>
-          <h2 className='text-center repo-name'>anime-recommendation</h2>
-          <div className='mt-2 flex gap-4 justify-center'>
-            <a href="https://github.com/chanasia/anime-recommendation" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
-            <a href="http://rengme.chanasia.site/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+      <motion.div
+        initial={{y:20, opacity:0}}
+        animate={{y:0, opacity:1}}
+        exit={{y:-20, opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+      <div className='h-screen overflow-hidden'>
+        <div className="mt-[120px] w-3/5 mx-auto grid grid-cols-3 gap-4">
+          <div className='border border-dashed rounded-lg p-4 h-[109px]'>
+            <h2 className='text-center repo-name'>anime-recommendation</h2>
+            <div className='mt-2 flex gap-4 justify-center'>
+              <a href="https://github.com/chanasia/anime-recommendation" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
+              <a href="http://rengme.chanasia.site/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+            </div>
           </div>
-        </div>
 
-        <div className='border border-dashed rounded-lg p-4 h-[109px]'>
-          <h2 className='text-center repo-name'>Covid-Interface</h2>
-          <div className='mt-2 flex gap-4 justify-center'>
-            <a href="https://github.com/chanasia/Covid-Interface" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
-            <a href="https://chanasia.github.io/Covid-Interface/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+          <div className='border border-dashed rounded-lg p-4 h-[109px]'>
+            <h2 className='text-center repo-name'>Covid-Interface</h2>
+            <div className='mt-2 flex gap-4 justify-center'>
+              <a href="https://github.com/chanasia/Covid-Interface" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
+              <a href="https://chanasia.github.io/Covid-Interface/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+            </div>
           </div>
-        </div>
 
-        <div className='border border-dashed rounded-lg p-4 h-[109px]'>
-          <h2 className='text-center repo-name'>wat-klai-ban</h2>
-          <div className='mt-2 flex gap-4 justify-center'>
-            <a href="https://github.com/chanasia/wat-klai-ban" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
-            <a href="https://chanasia.github.io/wat-klai-ban/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+          <div className='border border-dashed rounded-lg p-4 h-[109px]'>
+            <h2 className='text-center repo-name'>wat-klai-ban</h2>
+            <div className='mt-2 flex gap-4 justify-center'>
+              <a href="https://github.com/chanasia/wat-klai-ban" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Repo <DataObjectIcon /></a>
+              <a href="https://chanasia.github.io/wat-klai-ban/" className='border p-2 rounded-md hover:text-fuchsia-700 hover:border-fuchsia-700 transition-colors duration-200' target='_blank'>Web <LaunchIcon /></a>
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
+      </motion.div>
     </div>
   )
 }
